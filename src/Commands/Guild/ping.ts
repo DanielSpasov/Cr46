8cb596ca4b9265d79f72dc6e4b729f6c6673b6c0
Daniel Spasov <danielspasov4@gmail.com>
2021-09-24 13:28:05 +0300

    Modified ping command.

--- src/Commands/Guild/ping.ts
+++ src/Commands/Guild/ping.ts
@@ -1,11 +1,20 @@
+import { MessageEmbed } from 'discord.js';
+
 import { Command } from '../../Interfaces';
 
 
 
 export const command: Command = {
     name: 'ping',
-    aliases: ['p'], 
+    aliases: ['p'],
     run: async (client, message, args) => {
-        message.channel.send(`${client.ws.ping} ping!`);
+
+        const outputMessage = new MessageEmbed().setDescription(`\`${client.ws.ping}ms\``)
+
+        if (client.ws.ping <= 75) outputMessage.setColor('#78ff66')
+        if (client.ws.ping > 75 && client.ws.ping <= 150) outputMessage.setColor('#e6fa64')
+        if (client.ws.ping >= 150) outputMessage.setColor('#fa7664')
+
+        message.channel.send({ embeds: [outputMessage] });
     }
 }
