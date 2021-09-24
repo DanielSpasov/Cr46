c3c7424e2a3fe2da2f343165a1495768970edf93
Daniel Spasov <danielspasov4@gmail.com>
2021-09-24 13:30:12 +0300

    Set on ready activity.

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -6,5 +6,9 @@ export const event: Event = {
     name: 'ready',
     run: (client) => {
         console.log('Cr46 is Online!')
+        client.user.setActivity(
+            process.env.NODE_ENV === 'production' ? '@Cr46' : 'Under Maintenance',
+            { type: process.env.NODE_ENV === 'production' ? 'LISTENING' : 'WATCHING' }
+        )
     }
 }
