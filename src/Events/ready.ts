f509bc6f825e36dd8f77cc663cc04c178e20e7cb
Daniel Spasov <danielspasov4@gmail.com>
2021-09-27 11:03:26 +0300

    When the bot is ready he is now sending a message in the first valid channel.

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -1,14 +1,24 @@
+import { MessageEmbed, TextChannel } from 'discord.js';
 import { Event } from '../Interfaces';
 
 
 
 export const event: Event = {
     name: 'ready',
-    run: (client) => {
-        console.log('Cr46 is Online!')
+    run: async (client) => {
+
+        console.log('Cr46 is Online!');
+
         client.user.setActivity(
             process.env.NODE_ENV === 'production' ? '@Cr46' : 'Under Maintenance',
             { type: process.env.NODE_ENV === 'production' ? 'LISTENING' : 'WATCHING' }
-        )
+        );
+
+        const botCommandsChannel = await <Promise<TextChannel>>client.channels.fetch(client.config.valid_channels[0]);
+        botCommandsChannel.send({
+            embeds: [new MessageEmbed()
+                .setDescription(`Cr46 is Running in ${process.env.NODE_ENV} mode`)
+                .setColor('#78ff66')]
+        });
     }
 }
