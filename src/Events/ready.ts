16d78aca522e1df14608dc01a0c2b98469a4683b
Daniel Spasov <danielspasov4@gmail.com>
2021-11-15 19:54:40 +0200

    Removed role reactions.

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed, TextChannel } from 'discord.js';
+import { MessageEmbed, TextChannel } from 'discord.js';
 import { Event } from '../Interfaces';
 import connectDB from '../DB/config';
 
@@ -16,34 +16,6 @@ export const event: Event = {
 
             connectDB();
 
-            // SHUOLF NOT BE IN THE READY EVENT ---
-            const getEmoji = emojiName => client.emojis.cache.find(emoji => emoji.name === emojiName);
-            const emojis = {
-                league: 'League of Legends',
-                tft: 'TFT',
-                valorant: 'VALORANT',
-                minecraft: 'Minecraft',
-                gtav: 'Grand Theft Auto V',
-            }
-
-            let embedDescription = 'By reacting with a certain game emoji you will be notified everytime someone is looking to play and moved to the specific channel when playing the game.\n\n'
-            const reactions = []
-
-            for (const key in emojis) {
-                const emoji = getEmoji(key)
-                reactions.push(emoji)
-
-                const role = emojis[key]
-                embedDescription += `${emoji} --- ${role}\n`
-            }
-
-            const content = new MessageEmbed()
-                .setTitle('React to get the role.')
-                .setDescription(embedDescription)
-                .setColor('BLURPLE')
-
-            firstMessage(client, content, reactions);
-            
             console.log('Cr46 is Online!');
             const botCommandsChannel = await <Promise<TextChannel>>client.channels.fetch(client.config.valid_channels[0]);
             botCommandsChannel.send({
@@ -54,32 +26,4 @@ export const event: Event = {
 
         } catch (error) { console.log(error.message) }
     }
-}
-
-
-const firstMessage = async (client, content, reactions = []) => {
-    try {
-        const rolesChannel = await <Promise<TextChannel>>client.channels.fetch(client.config.roles_channel);
-        const messages = await rolesChannel.messages.fetch();
-
-        if (messages.size === 0) {
-            const message = await <Promise<Message>>rolesChannel.send({ embeds: [content] });
-            addReactions(message, reactions);
-        } else {
-            for (const msg of messages) {
-                msg[1].edit({ embeds: [content] });
-                addReactions(msg[1], reactions);
-            };
-        }
-    } catch (error) { console.log(error.message) }
-}
-
-const addReactions = (message, reactions) => {
-    try {
-        message.react(reactions[0]);
-        reactions.shift();
-        if (reactions.length > 0) {
-            setTimeout(() => addReactions(message, reactions), 750);
-        };
-    } catch(error) { console.log(error.message) }
 }
