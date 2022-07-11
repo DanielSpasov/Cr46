cf4cd007eb401e280e4ed8f85770d68964fb0e26
Daniel Spasov <danielspasov4@gmail.com>
2022-07-11 08:17:01 +0300

    Rearranged client config.

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -1,29 +1,24 @@
-import { Client, Collection } from "discord.js";
-import dotenv from "dotenv";
-
-import { Command, Event, Config } from "../Interfaces/Core";
-
 import setupCommandIntegrations from "./commandIntegration";
+import { Command, Event, Config } from "../Interfaces/Core";
+import { Client, Collection } from "discord.js";
 import setupCommands from "./commands";
 import setupEvents from "./events";
+import dotenv from "dotenv";
 
 dotenv.config();
 class ExtendedClient extends Client {
   public commands: Collection<string, Command> = new Collection();
   public events: Collection<string, Event> = new Collection();
   public aliases: Collection<string, Command> = new Collection();
-  public config: Config;
+  public config: Config = {
+    default_league_server: "euw1",
+    default_number_of_champs: 3,
+    startup_wallet_balance: 500,
+    ephemeral_commands: ["wallet", "setupwallet", "daily"],
+  };
 
   public async init() {
-    this.config = {
-      default_league_server: "euw1",
-      default_number_of_champs: 3,
-      startup_wallet_balance: 500,
-      ephemeral_commands: ["wallet", "setupwallet", "daily"],
-    };
-
     await this.login(process.env.BOT_TOKEN);
-
     setupCommands(this);
     setupEvents(this);
     setupCommandIntegrations(this);
